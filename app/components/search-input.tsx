f195882ae28064b99683b1ef2a67cc16d683ce69
jchapuis <jchapuis@nxt-jchapuis-mbp.local>
2017-06-27 09:14:03 +0200

    search input grid layout

--- app/components/search-input.tsx
+++ app/components/search-input.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Search, SearchResultProps } from "semantic-ui-react";
+import { Search, Header, Image, Grid, SearchResultProps } from "semantic-ui-react";
 
 export interface SearchSuggestionProps {
     readonly id: number;
@@ -34,17 +34,30 @@ export class SearchInput extends React.Component<SearchProps, any> {
 
     public render() {
         return (
-            <Search loading={this.props.loading}
-                results={this.props.suggestions}
-                onSearchChange={(evt, text) => this.props.inputChanged(text)}
-                onResultSelect={(evt, result) => this.props.suggestionSelected({
-                    id: result.id || 0, title: result.title || "", description: result.description || "", image: result.image || ""
-                })}
-                value={this.props.input}
-                open={this.props.menuOpen}
-                showNoResults={true}
-                fluid={true}
-            />
+            <Grid columns={1} centered>
+                <Grid.Row></Grid.Row>
+                <Grid.Row centered>
+                    <Grid.Column>
+                        <Image src="https://upload.wikimedia.org/wikipedia/en/8/80/Wikipedia-logo-v2.svg" centered size="small" />
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row centered>
+                    <Grid.Column width={10}>
+                        <Search loading={this.props.loading}
+                            input={{ fluid: true }}
+                            results={this.props.suggestions}
+                            onSearchChange={(evt, text) => this.props.inputChanged(text)}
+                            onResultSelect={(evt, result) => this.props.suggestionSelected({
+                                id: result.id || 0, title: result.title || "", description: result.description || "", image: result.image || ""
+                            })}
+                            value={this.props.input}
+                            open={this.props.menuOpen}
+                            showNoResults
+                            fluid
+                        />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         );
     }
 }
